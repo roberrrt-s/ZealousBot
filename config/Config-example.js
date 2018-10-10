314c4df91f9bbe9af9b1babf8bb7b013525ac1df
Robert Spier <robert.spier@gmail.com>
2018-10-10 18:00:09 +0200

    Update config example

--- config/Config-example.js
+++ config/Config-example.js
@@ -1,7 +1,8 @@
 const CONFIG = {
 	API_KEY: '<your api key>',
-	DEFAULT: '<your default bot channel>'
-	TIMEZONE: '<your timezone (Europe/London)>'
+	DEFAULT: '<your default bot channel>',
+	TIMEZONE: '<your timezone (Europe/London)>',
+	PREFIX: '<your command prefix'
 }
 
 module.exports = CONFIG;
