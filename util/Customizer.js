5b4bc5e7f7553b1b830bf424ef82eb76535833fb
Robert Spier <robert.spier@gmail.com>
2018-11-21 18:30:13 +0100

    Scratch that, we'll use redis

--- util/Customizer.js
+++ util/Customizer.js
@@ -4,20 +4,6 @@ const path = require('path');
 class Customizer {
 	constructor(app) {
 		this.app = app;
-
-		console.log(process.env.BOT_CHANNELS);
-
-		if(Boolean(process.env.BOT_CHANNELS)) {
-			process.env.BOT_CHANNELS = new Array();
-		}
-
-		let array = process.env.BOT_CHANNELS.split(",").map(Number);
-
-		array.push(4);
-
-		process.env.BOT_CHANNELS = array.toString();
-
-		console.log(process.env.BOT_CHANNELS);
 	}
 }
 
